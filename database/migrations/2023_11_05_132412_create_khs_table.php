ef476e3c613da331adaf174551b52d5cb168d235
imadearya <imadearya1@gmail.com>
2023-11-05 22:29:33 +0700

    lengkapi db

--- database/migrations/2023_11_05_132412_create_khs_table.php
+++ database/migrations/2023_11_05_132412_create_khs_table.php
@@ -15,6 +15,12 @@ class CreateKhsTable extends Migration
     {
         Schema::create('khs', function (Blueprint $table) {
             $table->id();
+            $table->string('nim')->nullable();
+            $table->string('semester')->nullable();
+            $table->string('jumlah_sks')->nullable();
+            $table->string('ips')->nullable();
+            $table->string('status')->nullable()->default('0');
+            $table->string('file')->nullable(); 
             $table->timestamps();
         });
     }
