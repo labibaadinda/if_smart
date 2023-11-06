50555f43f57c54ac4fe28bbbf0b4d325da9cc425
imadearya <imadearya1@gmail.com>
2023-11-06 22:43:04 +0700

    pkl seeder

--- database/seeders/PklSeeder.php
+++ database/seeders/PklSeeder.php
@@ -13,6 +13,19 @@ class PklSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('pkls')->insert([
+            [
+                'nim' => 'Irvan Gunawan, S.Si., M.T.',
+                'judul' => '00123456789',
+                'progres' => '1',
+                'stat_pkl' => '1',
+                'tanggal_mulai' => '1',
+            ],
+            [
+                'nama' => 'Deddy Cahyadi, S.Kom., M.T.',
+                'nip' => '00123456789',   
+            ],
+            ]);
+        }
     }
 }
