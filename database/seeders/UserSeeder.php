9c2da66f5a1f87e9db131eebc0a2326cbb100e90
imadearya <imadearya1@gmail.com>
2023-11-06 15:30:19 +0700

    import class DB dan Hash

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use DB;
-use Hash;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
