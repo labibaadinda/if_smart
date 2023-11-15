ce3302d93b7b08736815fb18a24a683e4ac7d400
labib <labibhs01@gamil.com>
2023-11-15 11:58:15 +0700

    Departemen not fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,5 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'user-role' => \App\Http\Middleware\UserRole::class,
-
     ];
 }
